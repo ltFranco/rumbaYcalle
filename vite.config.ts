9cd1bbac88ddc5ed6ba16db32ce2c76b92b7407b
ltFranco <149882390+ltFranco@users.noreply.github.com>
2025-11-23 22:26:45 -0400

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // ¡AÑADE ESTA LÍNEA!
+  base: './', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
